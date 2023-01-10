b2e3010a573d8479fb2b30f2cac0fd4a9a2b6ae9
David Vo <auscompgeek@users.noreply.github.com>
2023-01-10 19:56:33 +1100

    pyfrc_test: Enable disabled and teleop tests

--- tests/pyfrc_test.py
+++ tests/pyfrc_test.py
@@ -1,14 +1,13 @@
 # NB: autonomous is tested separately in autonomous_test.py
-from pyfrc.tests import (  # type: ignore
-    # Running all the tests seems to segfault, but a subset seems okay?
-    # test_disabled,
-    # test_operator_control,
+from pyfrc.tests import (
+    test_disabled,
+    test_operator_control,
     test_practice,
 )
 
 # Make pyflakes happy about our imports.
 __all__ = (
-    # "test_disabled",
-    # "test_operator_control",
+    "test_disabled",
+    "test_operator_control",
     "test_practice",
 )
