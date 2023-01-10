766a8359ed1c93a83973fa68bc6622d12326550b
OliverW10 <oliver.warrick@thedropbears.org.au>
2023-01-10 15:25:00 +1100

    added basic automove

--- controllers/movement.py
+++ controllers/movement.py
@@ -1,7 +1,8 @@
 from magicbot import StateMachine, state, timed_state, default_state
 from wpimath.geometry import Pose2d
 from components.chassis import Chassis
-
+from wpimath.controller import ProfiledPIDController
+from wpimath.trajectory import TrapezoidProfile
 
 class Movement(StateMachine):
     chassis: Chassis
@@ -10,6 +11,9 @@ class Movement(StateMachine):
         self.goal = Pose2d()
         self.inputs = (0, 0, 0)
         self.drive_local = False
+        self.x_controller = ProfiledPIDController(3, 0, 0, TrapezoidProfile.Constraints(1,1))
+        self.y_controller = ProfiledPIDController(3, 0, 0, TrapezoidProfile.Constraints(1,1))
+        self.heading_controller = ProfiledPIDController(3, 0, 0, TrapezoidProfile.Constraints(1,1))
 
     # will execute if no other states are executing
     @default_state
@@ -21,6 +25,16 @@ class Movement(StateMachine):
 
     @state(first=True)
     def autodrive(self):
+        self.x_controller.setGoal(self.goal.X())
+        self.y_controller.setGoal(self.goal.Y())
+        self.heading_controller.setGoal(self.goal.rotation().radians())
+
+        x_velocity = self.x_controller.calculate(self.chassis.get_pose().X())
+        y_velocity = self.y_controller.calculate(self.chassis.get_pose().Y())
+        omega = self.heading_controller.calculate(self.chassis.get_pose().rotation().radians())
+        
+        self.chassis.drive_field(x_velocity,y_velocity,omega)
+
         """Drive to a waypoint"""
         ...
 
