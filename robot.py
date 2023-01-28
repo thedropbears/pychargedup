f8ab73e85c41624d4ca5f47e415d273d49fb5827
Amber Knights <amber.knights@thedropbears.org.au>
2023-01-28 16:06:48 +1100

    made objects for a double solenoid and the motor

--- robot.py
+++ robot.py
@@ -27,6 +27,14 @@ class MyRobot(magicbot.MagicRobot):
 
         self.field = wpilib.Field2d()
         wpilib.SmartDashboard.putData(self.field)
+        self.intake_motor = CANSparkMax(
+            ids.CanIds.Intake.intake_motor, CANSparkMax.MotorType.kBrushless
+        )
+        self.intake_piston = DoubleSolenoid(
+            wpilib.PneumaticsModuleType.CTREPCM,
+            ids.PcmChannels.Intake.intake_piston_forward,
+            ids.PcmChannels.Intake.intake_piston_reverse,
+        )
 
     def teleopPeriodic(self) -> None:
         drop_off = self.gamepad.getAButton()
