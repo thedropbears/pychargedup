3170678b0df03d665e811e0ce8df1d3f50f6d75e
Lucien Morey <lucien@spacemachines.co>
2023-01-29 15:51:53 +1100

    constucting hardware inside component

--- components/intake.py
+++ components/intake.py
@@ -1,23 +1,30 @@
 from rev import CANSparkMax
-from wpilib import DoubleSolenoid
+from wpilib import DoubleSolenoid, PneumaticsModuleType
 from magicbot import tunable
+import ids
 
 
 class Intake:
-    intake_motor: CANSparkMax
-    intake_piston: DoubleSolenoid
     intake_speed = tunable(0.5)
 
     def __init__(self):
         self.deployed = False
+        self.motor = CANSparkMax(
+            ids.CanIds.Intake.intake_motor, CANSparkMax.MotorType.kBrushless
+        )
+        self.piston = DoubleSolenoid(
+            PneumaticsModuleType.CTREPCM,
+            ids.PcmChannels.Intake.intake_piston_forward,
+            ids.PcmChannels.Intake.intake_piston_reverse,
+        )
 
     def execute(self):
         if self.deployed:
-            self.intake_piston.set(DoubleSolenoid.Value.kForward)
-            self.intake_motor.set(self.intake_speed)
+            self.piston.set(DoubleSolenoid.Value.kForward)
+            self.motor.set(self.intake_speed)
         else:
-            self.intake_piston.set(DoubleSolenoid.Value.kReverse)
-            self.intake_motor.set(0.0)
+            self.piston.set(DoubleSolenoid.Value.kReverse)
+            self.motor.set(0.0)
 
     def deploy(self):
         self.deployed = True
