6c30cb972e8ccd85e9aea86d21bdf31801c99514
Pokesi <81540683+Pokesi@users.noreply.github.com>
2023-02-05 16:44:42 +1100

    begin LED controller

--- controllers/leds.py
+++ controllers/leds.py
@@ -1,5 +1,40 @@
-import components.leds
+from components.leds import *, LedColors, DisplayType
+from components.gripper import Gripper
+from components.intake import Intake
 from magicbot import feedback
 
 class LedController:
-    ...
+    status_lights: StatusLights
+    gripper: Gripper
+    intake: Intake
+
+    def __init__(self) -> None:
+        ...
+
+    def execute(self) -> None:
+        CONE = self.gripper.get_full_closed() and CHECK_THAT_PIECE_ISNT_A_CUBE
+        CUBE = self.intake.is_game_piece_present()
+        LEFT = LEFT_CHECK
+        RIGHT = RIGHT_CHECK
+        WANTS_CONE = WANTS_CONE_CHECK
+        WANTS_CUBE = WANTS_CUBE_CHECK
+        if WANTS_CONE:
+            if LEFT:
+                self.status_lights.want_cone_left()
+            elif RIGHT:
+                self.status_lights.want_cone_right()
+        elif WANTS_CUBE:
+            if LEFT:
+                self.status_lights.want_cube_left()
+            elif RIGHT:
+                self.status_lights.want_cube_right()
+        elif CONE:
+            self.status_lights.cone_onboard()
+        elif CUBE:
+            self.status_lights.cube_onboard()
+        else:
+            self.status_lights.set_display_pattern(DisplayType.SOLID)
+            self.status_lights.set_color(LedColors.OFF)
+
+        
+        
