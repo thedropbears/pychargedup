e07d28da1b94df90fae6d0377024e4fd79303a21
OliverW10 <oliver.warrick@thedropbears.org.au>
2023-02-11 22:20:56 +1100

    added swap_substation and fixed goal rotation

--- controllers/scoring.py
+++ controllers/scoring.py
@@ -1,5 +1,5 @@
 from magicbot import state, StateMachine, tunable, feedback
-from wpimath.geometry import Pose2d, Rotation2d, Translation3d
+from wpimath.geometry import Pose2d, Rotation2d, Translation2d
 import wpilib
 
 from components.gripper import Gripper
@@ -8,10 +8,10 @@ from components.arm import Arm, Setpoints, Setpoint
 from controllers.movement import Movement
 from utilities.game import (
     GamePiece,
+    field_flip_pose2d,
     get_double_substation,
     get_single_substation,
     field_flip_rotation2d,
-    RED_NODES,
     BLUE_NODES,
     Rows,
 )
@@ -33,8 +33,12 @@ class ScoringController(StateMachine):
     PICKUP_CUBE_PREPARE_TIME = tunable(3)
     SCORE_PREPARE_TIME = tunable(3)
     PICKUP_CONE_PREPARE_TIME = tunable(3)
+
     DESIRED_ROW = tunable(0)
     DESIRED_COLUMN = tunable(0)
+
+    # swap the the side of the substation to test on a half field
+    swap_substation = tunable(False)
 
     def __init__(self) -> None:
         self.is_holding = GamePiece.NONE
@@ -43,7 +47,6 @@ class ScoringController(StateMachine):
         self.wants_to_intake = False
         # use double substation shelf on right side from drivers pov
         self.cone_pickup_side_right = False
-        self.target_nodes = RED_NODES if self.is_red() else BLUE_NODES
 
     def get_correct_autodrive_state(self) -> str:
         cur_piece = self.get_current_piece()
@@ -185,50 +188,53 @@ class ScoringController(StateMachine):
         return Pose2d(goal_trans, goal_rotation), goal_rotation
 
     def get_cone_pickup(self) -> tuple[Pose2d, Rotation2d]:
+        # use the shelf closest to the wall, furthest from the nodes
         is_wall_side = self.cone_pickup_side_right == self.is_red()
-        goal_trans = get_double_substation(self.get_team(), is_wall_side)
+        # if we want the substation to be as if we are on the red alliance
+        is_red = self.is_red() != self.swap_substation
+        cone_trans = get_double_substation(is_red, is_wall_side).toTranslation2d()
 
-        goal_rotation = (
-            field_flip_rotation2d(Rotation2d(0)) if self.is_red() else Rotation2d(0)
-        )
+        # as if we're blue
+        goal_rotation = Rotation2d.fromDegrees(180)
+        goal_approach = Rotation2d(0)
         offset_x, _ = Setpoints.PICKUP_CONE.toCartesian()
+        if is_red:
+            offset_x *= -1
+            goal_rotation = field_flip_rotation2d(goal_rotation)
+            goal_approach = field_flip_rotation2d(goal_approach)
+        goal_trans = cone_trans + Translation2d(offset_x, 0)
 
-        # offset goal_trans to be where robot centre will be
-        if self.is_red():
-            goal_trans = goal_trans + Translation3d(-offset_x, 0, 0)
-        else:
-            goal_trans = goal_trans + Translation3d(offset_x, 0, 0)
         return (
             Pose2d(
-                goal_trans.toTranslation2d(),
-                goal_rotation + Rotation2d.fromDegrees(180),
+                goal_trans,
+                goal_rotation,
             ),
-            goal_rotation,
+            goal_approach,
         )
 
     def get_score_location(self) -> tuple[tuple[Pose2d, Rotation2d], Setpoint]:
         # TODO: pick actual scoring node
-        goal_trans = self.target_nodes[int(self.DESIRED_ROW)][int(self.DESIRED_COLUMN)]
+        node_trans3d = BLUE_NODES[int(self.DESIRED_ROW)][int(self.DESIRED_COLUMN)]
+        node_trans = node_trans3d.toTranslation2d()
 
+        setpoint = Setpoints.SCORE_CONE_HIGH
         if self.get_current_piece() == GamePiece.CONE:
             if self.DESIRED_ROW == Rows.HIGH:
-                offset_x, _ = Setpoints.SCORE_CONE_HIGH.toCartesian()
+                setpoint = Setpoints.SCORE_CONE_HIGH
             elif self.DESIRED_ROW == Rows.MID:
-                offset_x, _ = Setpoints.SCORE_CONE_MID.toCartesian()
+                setpoint = Setpoints.SCORE_CONE_MID
         elif self.get_current_piece() == GamePiece.CUBE:
             if self.DESIRED_ROW == Rows.HIGH:
-                offset_x, _ = Setpoints.SCORE_CUBE_HIGH.toCartesian()
+                setpoint = Setpoints.SCORE_CUBE_HIGH
             elif self.DESIRED_ROW == Rows.MID:
-                offset_x, _ = Setpoints.SCORE_CUBE_MID.toCartesian()
+                setpoint = Setpoints.SCORE_CUBE_MID
 
+        offset_x, _ = setpoint.toCartesian()
+        goal_trans = node_trans + Translation2d(-offset_x, 0)
+        goal = Pose2d(goal_trans, Rotation2d(0))
+        goal_approach = Rotation2d.fromDegrees(180)
         if self.is_red():
-            goal_trans = goal_trans + Translation3d(+offset_x, 0, 0)
-        else:
-            goal_trans = goal_trans + Translation3d(-offset_x, 0, 0)
+            goal = field_flip_pose2d(goal)
+            goal_approach = field_flip_rotation2d(goal_approach)
 
-        goal_rot = Rotation2d.fromDegrees(180)
-        goal_approach = Rotation2d.fromDegrees(0)
-        return (
-            Pose2d(goal_trans.toTranslation2d(), goal_rot),
-            goal_approach,
-        ), Setpoints.SCORE_CONE_HIGH
+        return (goal, goal_approach), setpoint
