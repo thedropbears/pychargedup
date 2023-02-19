8af66f6c8a7916aa2faf19b85788d06bfa8b99ee
Lucien Morey <lucien@spacemachines.co>
2023-02-19 17:05:53 +1100

    starting leds in rainbow mode

--- components/leds.py
+++ components/leds.py
@@ -170,9 +170,9 @@ class StatusLights:
 
         self.start_time = time.monotonic()
 
-        self.color = (0, 0, 0)
+        self.color = LedColors.CYAN.value
 
-        self.pattern = DisplayType.SOLID
+        self.pattern = DisplayType.RAINBOW
 
         self.side = PickupFromSide.RIGHT
 
@@ -186,6 +186,7 @@ class StatusLights:
         self.choose_morse_message()
         self.leds.setLength(self.led_length)
         self.single_led_data = wpilib.AddressableLED.LEDData()
+        self.single_led_data.setHSV(*self.color)
         self.leds_data = [self.single_led_data] * self.led_length
         self.leds.setData(self.leds_data)
         self.leds.start()
