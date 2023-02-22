48da9769dc0e9d8562d898ad71d4d5e243ac9fc6
OliverW10 <oliver.warrick@thedropbears.org.au>
2023-02-23 09:31:21 +1100

    removed gripper switch

--- components/gripper.py
+++ components/gripper.py
@@ -1,4 +1,4 @@
-from wpilib import DoubleSolenoid, DigitalInput, PneumaticsModuleType
+from wpilib import DoubleSolenoid, PneumaticsModuleType
 from magicbot import feedback
 import time
 
@@ -13,7 +13,6 @@ class Gripper:
         self.opened = False
         self.last_opened = self.opened
         self.change_time = time.monotonic()
-        self.wants_to_close = False
 
         self.solenoid = DoubleSolenoid(
             PneumaticsModuleType.REVPH,
@@ -21,7 +20,6 @@ class Gripper:
             ids.PhChannels.gripper_solenoid_reverse,
         )
 
-        self.game_piece_switch = DigitalInput(ids.DioChannels.gripper_game_piece_switch)
         self.set_solenoid = False
 
     def open(self) -> None:
@@ -50,9 +48,6 @@ class Gripper:
         return self.opened and not self.get_full_open()
 
     def execute(self) -> None:
-        if self.wants_to_close and self.game_piece_in_reach():
-            self.opened = False
-
         if self.opened != self.last_opened:
             self.change_time = time.monotonic()
         self.last_opened = self.opened
@@ -62,7 +57,3 @@ class Gripper:
                 self.solenoid.set(DoubleSolenoid.Value.kReverse)
             else:
                 self.solenoid.set(DoubleSolenoid.Value.kForward)
-
-    @feedback
-    def game_piece_in_reach(self) -> bool:
-        return not self.game_piece_switch.get()
