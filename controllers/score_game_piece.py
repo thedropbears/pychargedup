dbaa95f2154f09c5778463e7c4050fddbf6688d9
OliverW10 <oliver.warrick@thedropbears.org.au>
2023-02-23 11:16:53 +1100

    add score game piece controller

--- controllers/score_game_piece.py
+++ controllers/score_game_piece.py
@@ -1,4 +1,4 @@
-from components.arm import Arm
+from components.arm import Arm, get_setpoint_from_node
 from components.intake import Intake
 from components.gripper import Gripper
 
@@ -6,6 +6,14 @@ from controllers.movement import Movement
 from controllers.recover import RecoverController
 
 from magicbot import state, StateMachine
+from enum import Enum, auto
+from utilities.game import Node, get_closest_node, get_score_location, Rows
+
+
+class NodePickStratergy(Enum):
+    CLOSEST = auto()
+    OVERRIDE = auto()
+    BEST = auto()
 
 
 class ScoreGamePieceController(StateMachine):
@@ -16,18 +24,40 @@ class ScoreGamePieceController(StateMachine):
     movement: Movement
     recover: RecoverController
 
+    def __init__(self) -> None:
+        self.node_stratergy = NodePickStratergy.CLOSEST
+        self.override_node = Node(Rows.HIGH, 0)
+        self.target_node = Node(Rows.HIGH, 0)
+
     @state(first=True, must_finish=True)
-    def driving_to_position(self):
-        self.next_state("deploying_arm")
+    def driving_to_position(self, initial_call):
+        if initial_call:
+            self.target_node = self.pick_node()
+        self.movement.set_goal(*get_score_location(self.target_node))
+        if self.movement.is_at_goal():
+            self.next_state("deploying_arm")
 
     @state(must_finish=True)
     def deploying_arm(self):
-        self.next_state("dropping")
+        self.arm.go_to_setpoint(get_setpoint_from_node(self.target_node))
+        if self.arm.at_goal():
+            self.next_state("dropping")
 
     @state(must_finish=True)
     def dropping(self):
-        self.done()
+        self.gripper.open()
+        if self.gripper.get_full_open():
+            self.done()
 
     def done(self) -> None:
         super().done()
         self.recover.engage()
+
+    def pick_node(self) -> Node:
+        cur_pos = self.movement.chassis.get_pose().translation()
+        if self.node_stratergy is NodePickStratergy.CLOSEST:
+            return get_closest_node(cur_pos, self.gripper.get_current_piece())
+        elif self.node_stratergy is NodePickStratergy.OVERRIDE:
+            return self.override_node
+        elif self.node_stratergy is NodePickStratergy.BEST:
+            return get_closest_node(cur_pos, self.gripper.get_current_piece())
