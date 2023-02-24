2e0f3c147ded6ae44ae60e170197bcc6f19814ea
OliverW10 <oliver.warrick@thedropbears.org.au>
2023-02-24 15:50:50 +1100

    fix autonomous

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -4,15 +4,15 @@ from dataclasses import dataclass
 
 from components.chassis import Chassis
 from components.intake import Intake
-from components.arm import Arm, Setpoints, Setpoint
 from components.gripper import Gripper
-
+from controllers.arm import ArmController, Setpoint, Setpoints
 from controllers.movement import Movement
-
-from wpilib import Timer
-from wpimath.geometry import Pose2d, Rotation2d, Translation2d
+from controllers.recover import RecoverController
 
 from utilities.functions import grid_col_to_field_y
+
+from wpimath.geometry import Pose2d, Rotation2d, Translation2d
+
 
 from math import radians
 
@@ -34,24 +34,23 @@ class ScorePath:
 
 class AutoBase(AutonomousStateMachine):
     gripper: Gripper
-    arm: Arm
+    arm: ArmController
     chassis: Chassis
     intake: Intake
     movement: Movement
+    recover: RecoverController
+
+    INTAKE_PRE_TIME = 2.0
 
     def __init__(self) -> None:
-        self.piece_positions = []
         self.pickup_paths = []
         self.score_paths = []
-        self.stage = 0
-        self.timeout_set = False
-        self.timeout_start = 0
+        self.progress_idx = 0
 
     @state(first=True)
-    def score(self, state_tm: float, initial_call: bool) -> None:
+    def score(self, initial_call: bool) -> None:
         if initial_call:
-            self.timeout_set = False
-            path = self.score_paths[self.stage]
+            path = self.score_paths[self.progress_idx]
             self.movement.next_state("autodrive")
             self.movement.set_goal(
                 path.goal,
@@ -59,26 +58,22 @@ class AutoBase(AutonomousStateMachine):
                 waypoints=path.intermediate_waypoints,
             )
         self.movement.do_autodrive()
-        if self.movement.time_remaining < 0.5:
-            # self.arm.set_setpoint(self.score_paths[self.stage].arm_setpoint)
-            pass
-        if (
-            self.movement.is_at_goal()
-            # and self.arm.at_goal_angle()
-            # and self.arm.at_goal_extension()
-        ):
+        if self.movement.time_to_goal < 2:
+            self.arm.go_to_setpoint(self.score_paths[self.progress_idx].arm_setpoint)
+        if self.movement.is_at_goal() and self.arm.at_goal():
             self.gripper.open()
-        if self.gripper.opened:
-            # self.arm.set_setpoint(Setpoints.HANDOFF)
-            if self.stage >= len(self.pickup_paths):
+
+        if self.gripper.get_full_open():
+            if self.progress_idx >= len(self.pickup_paths):
                 self.done()
+                return
+            self.recover.engage()
             self.next_state("pickup_cube")
 
     @state
-    def pickup_cube(self, state_tm: float, initial_call: bool) -> None:
+    def pickup_cube(self, initial_call: bool) -> None:
         if initial_call:
-            self.timeout_set = False
-            path = self.pickup_paths[self.stage]
+            path = self.pickup_paths[self.progress_idx]
             self.movement.next_state("autodrive")
             self.movement.set_goal(
                 path.goal,
@@ -86,23 +81,16 @@ class AutoBase(AutonomousStateMachine):
                 waypoints=path.intermediate_waypoints,
             )
         self.movement.do_autodrive()
-        if self.movement.time_remaining < 0.5:
-            if state_tm > 0.1 and not self.timeout_set:
-                self.timeout_set = True
-                self.timeout_start = Timer.getFPGATimestamp()
+        if self.movement.time_to_goal < self.INTAKE_PRE_TIME:
+            self.arm.go_to_setpoint(Setpoints.HANDOFF)
             self.intake.deploy()
-        if (
-            self.movement.is_at_goal()
-            and self.intake.is_game_piece_present()
-            or (
-                self.timeout_set and Timer.getFPGATimestamp() - self.timeout_start > 2.0
-            )
-        ):
-            self.intake.retract()
+
+        if self.movement.is_at_goal():
             self.gripper.close()
             if self.gripper.get_full_closed():
-                self.stage += 1
+                self.progress_idx += 1
                 self.next_state("score")
+                self.recover.engage()
 
 
 class AutoTest(AutoBase):
@@ -114,19 +102,19 @@ class AutoTest(AutoBase):
             ScorePath(
                 Pose2d(1.88795, grid_col_to_field_y(0), Rotation2d(0.0)),
                 Rotation2d.fromDegrees(180.0),
-                None,
+                [],
                 Setpoints.SCORE_CONE_HIGH,
             ),
             ScorePath(
                 Pose2d(1.88795, grid_col_to_field_y(1), Rotation2d(0.0)),
                 Rotation2d.fromDegrees(180.0),
-                None,
+                [],
                 Setpoints.SCORE_CUBE_HIGH,
             ),
             ScorePath(
                 Pose2d(1.88795, grid_col_to_field_y(4), Rotation2d(0.0)),
                 Rotation2d.fromDegrees(180.0),
-                None,
+                [],
                 Setpoints.SCORE_CUBE_HIGH,
             ),
         ]
@@ -134,11 +122,11 @@ class AutoTest(AutoBase):
             PickupPath(
                 Pose2d(6.65885, 0.88812, Rotation2d(radians(4.56))),
                 Rotation2d.fromDegrees(4.56),
-                None,
+                [],
             ),
             PickupPath(
                 Pose2d(6.719, 1.92247, Rotation2d(radians(32.04))),
                 Rotation2d.fromDegrees(32.04),
-                None,
+                [],
             ),
         ]
