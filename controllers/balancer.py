baa309fdc312142cd1a3c9dc1a165dd2f2c712cd
DensityInfinite <douglas_jiang@hotmail.com>
2023-03-03 17:49:20 +1100

    Added basic balancer logic

--- controllers/balancer.py
+++ controllers/balancer.py
@@ -2,10 +2,12 @@ import math
 from magicbot import StateMachine, state
 
 from controllers.movement import Movement
+from components.chassis import Chassis
 
 
 class ChargeStation(StateMachine):
     movement: Movement
+    chassis: Chassis
 
     DRIVE_ON_SPEED = 0.5
     BALANCE_SPEED = 0.3
@@ -14,15 +16,31 @@ class ChargeStation(StateMachine):
     def __init__(self) -> None:
         self.drive_direction_positive = True
 
-    @state
+    @state(first=True)
     def drive_on(self) -> None:
         """Drive until we detect we are on the station"""
-        ...
+        self.chassis.drive_field(
+            self.DRIVE_ON_SPEED
+            if self.drive_direction_positive
+            else -self.DRIVE_ON_SPEED,
+            0,
+            0,
+        )
+        if abs(self.chassis.get_tilt()) > self.LEVEL_THRESHOLD:
+            self.next_state("balance")
 
     @state
     def balance(self) -> None:
         """Drive until the station tilts"""
-        ...
+        self.chassis.drive_field(
+            self.BALANCE_SPEED
+            if self.drive_direction_positive
+            else -self.BALANCE_SPEED,
+            0,
+            0,
+        )
+        if abs(self.chassis.get_tilt()) < self.LEVEL_THRESHOLD:
+            self.done()
 
     def start(self, positive_x: bool) -> None:
         self.engage()
