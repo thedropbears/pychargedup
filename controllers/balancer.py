12c4628fca094f9676ac4df31f02c3b5d8be28c8
DensityInfinite <douglas_jiang@hotmail.com>
2023-03-04 12:01:02 +1100

    Now assumes robot is on the charge station

--- controllers/balancer.py
+++ controllers/balancer.py
@@ -1,22 +1,26 @@
 import math
 from magicbot import StateMachine, state
+from utilities.game import get_team
+from wpimath.filter import Debouncer
 
 from components.chassis import Chassis
-from utilities.game import get_team
 
 
 class ChargeStation(StateMachine):
     chassis: Chassis
 
     DRIVE_ON_SPEED = 0.5
-    BALANCE_SPEED = 0.3
-    LEVEL_THRESHOLD = math.radians(7)
+    BALANCE_SPEED = 0.2
+    LEVEL_THRESHOLD = math.radians(4)
+    TILT_RATE_THRESHOLD = math.radians(10)
+    DEBOUNCE_TIME = 0.5
 
     BLUE_CHARGE_STATION_X = 3.15165
     RED_CHARGE_STATION_X = 12.70255
 
     def __init__(self) -> None:
         self.drive_direction_positive = True
+        self.debouncer = Debouncer(self.DEBOUNCE_TIME, Debouncer.DebounceType.kRising)
 
     def get_direction(self):
         team = get_team().name
@@ -27,31 +31,18 @@ class ChargeStation(StateMachine):
             self.drive_direction_positive = pose.X() < self.RED_CHARGE_STATION_X
 
     @state(first=True)
-    def drive_on(self, initial_call) -> None:
-        """Drive until we detect we are on the station"""
-        if initial_call:
-            self.get_direction()
-        self.chassis.drive_field(
-            self.DRIVE_ON_SPEED
-            if self.drive_direction_positive
-            else -self.DRIVE_ON_SPEED,
-            0,
-            0,
-        )
-        if abs(self.chassis.get_tilt()) > self.LEVEL_THRESHOLD:
-            self.next_state("balance")
-
-    @state
     def balance(self) -> None:
         """Drive until the station tilts"""
-        self.chassis.drive_field(
-            self.BALANCE_SPEED
-            if self.drive_direction_positive
-            else -self.BALANCE_SPEED,
+        tilt = self.chassis.get_tilt()
+        rate = self.chassis.get_tilt_rate()
+
+        self.chassis.drive_local(
+            self.BALANCE_SPEED if tilt < 0 else -self.BALANCE_SPEED,
             0,
             0,
         )
-        if abs(self.chassis.get_tilt()) < self.LEVEL_THRESHOLD:
+
+        if self.debouncer.calculate(abs(rate) >= self.TILT_RATE_THRESHOLD):
             self.done()
 
     def start(self) -> None:
